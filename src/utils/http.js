73bae5b5a1f569b2c72ca795e50f035c43b49349
z891220z <z891220z@gmail.com>
2024-04-22 21:45:08 +0800

    帳號功能 - 登入逾時警告

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,8 +1,10 @@
 import axios from "axios";
-import {ElMessage} from "element-plus";
+import {ElMessage,ElMessageBox} from "element-plus";
 import 'element-plus/theme-chalk/el-message.css'
+import 'element-plus/theme-chalk/index.css'
 import { useUserStore } from '@/stores/userStore'
 import { useRouter } from 'vue-router';
+import router from '@/router';
 
 const http = axios.create({
     baseURL:'http://localhost:8080',
@@ -25,11 +27,11 @@ http.interceptors.response.use(res => res.data, e => {
     //統一錯誤提示
     ElMessage({type: 'error', message: e.response.data})
     //401token失效處理
-    const userStore = useUserStore();
-    const router = useRouter();
+    //const router = useRouter();
     if(e.response.status === 401){
-        //userStore.clearUserInfo()
-        //router.push('/login')
+        ElMessageBox.alert('登入逾時 或 登入帳號權限不足，請重新登入', '登入異常警告', {
+            confirmButtonText: 'OK'
+        }).then(() => {router.push('/login')})
     }
     return Promise.reject(e)
 })
